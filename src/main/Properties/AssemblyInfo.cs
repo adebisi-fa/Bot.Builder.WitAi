825cd5553ca752513d5478b8f1faf3c6793a7956
adebisi-fa-hp <adebisi-fa@live.com>
2017-01-08 00:05:47 +0100

    Initial Commit.

---
+++ src/main/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Adfa.Bot.Builder.Luis.WitAi")]
+[assembly: AssemblyDescription("Wit.ai integration with Microsoft Bot Builder LuisDialog.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Adfa")]
+[assembly: AssemblyProduct("Adfa.Bot.Builder.Luis.WitAi")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30414c95-49a0-4f39-ac6f-49c2cb1532e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
